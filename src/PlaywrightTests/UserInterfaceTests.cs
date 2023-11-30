e6a9635e336531da51b83d217ddd0fc56507935d
Williston77 <152225282+Williston77@users.noreply.github.com>
2023-11-29 17:38:49 -0800

    Update UserInterfaceTests.cs
    first attempt at successful login for Test2

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -21,23 +21,24 @@ public class UserInterfaceTests
 
         IPage page = await context.NewPageAsync();
         //Navigate to Google.com
-        await page.GotoAsync("https://www.google.com");
-        IReadOnlyList<IFrame> f = page.Frames;
-        if (f.Count > 1)
-        {
-            await f[1].ClickAsync("text=No thanks");
-        }
+        await page.GotoAsync("https://letsusedata.com/");
+        //IReadOnlyList<IFrame> f = page.Frames;
+        //if (f.Count > 1)
+        //{
+        //    await f[1].ClickAsync("text=No thanks");
+        //}
         // Search Playwright
-        await page.FillAsync("[aria-label=\"Search\"]", "Playwright");
+        await page.FillAsync("id=txtUser", "Test2");
+        await page.FillAsync("id=txtPassword", "iF3sBF7c");
         // Press Enter
-        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
+        // var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
         //Click on the first search option
-        await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
+        // await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
         //Verify Page URL
-        Assert.Equal("https://playwright.dev/", page.Url);
+        //Assert.Equal("https://playwright.dev/", page.Url);
         // Click text=Get started
-        await page.ClickAsync("text=Get Started");
+        await page.ClickAsync("id=javascriptLogin");
         //Verify Page URL
-        Assert.Equal("https://playwright.dev/docs/intro", page.Url);
+        Assert.Equal("https://letsusedata.com/CourseSelection.html", page.Url);
     }
 }
