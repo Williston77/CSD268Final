22905379ae679f462516929f7efea03d58fbac83
Williston77 <152225282+Williston77@users.noreply.github.com>
2023-11-29 18:02:08 -0800

    Update UserInterfaceTests.cs
    reverting back to original to see if original is the problem

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -20,22 +20,24 @@ public class UserInterfaceTests
         IBrowserContext context = await browser.NewContextAsync();
 
         IPage page = await context.NewPageAsync();
-        //Navigate to letsusedata.com
-        await page.GotoAsync("https://letsusedata.com/");
-        // enter in username and password
-        await page.FillAsync("id=txtUser", "Test1");
-        await page.FillAsync("id=txtPassword", "12345678");
-        // press login
-        await page.ClickAsync("id=javascriptLogin");
-        // verify we have not changed page
-        Assert.Equal("https://letsusedata.com/", page.Url);
-        // time for round 2 which should succeed
-        // enter in username and password
-        await page.FillAsync("id=txtUser", "Test2");
-        await page.FillAsync("id=txtPassword", "iF3sBF7c");
-        // Press login
-        await page.ClickAsync("id=javascriptLogin");
+        //Navigate to Google.com
+        await page.GotoAsync("https://www.google.com");
+        IReadOnlyList<IFrame> f = page.Frames;
+        if (f.Count > 1)
+        {
+            await f[1].ClickAsync("text=No thanks");
+        }
+        // Search Playwright
+        await page.FillAsync("[aria-label=\"Search\"]", "Playwright");
+        // Press Enter
+        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
+        //Click on the first search option
+        await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
         //Verify Page URL
-        Assert.Equal("https://letsusedata.com/CourseSelection.html", page.Url);
+        Assert.Equal("https://playwright.dev/", page.Url);
+        // Click text=Get started
+        await page.ClickAsync("text=Get Started");
+        //Verify Page URL
+        Assert.Equal("https://playwright.dev/docs/intro", page.Url);
     }
 }
